aad5e2e442f6b75b801d4403e6c06b3358668e62
uvaldez <uzielvaldez85@gmail.com>
2024-05-13 08:06:42 -0500

    feat: use class for food truck service

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { isNumber } from './utils/index.js';
-import { getAllFoodTrucks, getFoodTruckById, searchFoodTruck } from './services/foodTruck/index.js';
+import FoodTruck from './services/foodTruck/index.js';
 
 const app = express();
 const PORT = 8080;
@@ -14,6 +14,7 @@ app.listen(
   () => console.log(`Application running at port: ${PORT}`),
 );
 
+const foodTruckService = new FoodTruck();
 /**
  * /food-trucks?limit=100&offset=0
  * Get all food trucks
@@ -27,7 +28,7 @@ app.get('/food-trucks', async (req, res) => {
     return;
   }
   try {
-    const data = await getAllFoodTrucks(offset, limit);
+    const data = await foodTruckService.getAllFoodTrucks(offset, limit);
     res.status(200).send(data);
     return;
   } catch(e){
@@ -43,7 +44,7 @@ app.get('/food-trucks', async (req, res) => {
 app.get('/food-trucks/:id', async (req, res) => {
   const { id } = req.params;
   try {
-    const data = await getFoodTruckById(id);
+    const data = await foodTruckService.getFoodTruckById(id);
     if (!data) {
       res.status(404).send({
         message: `Food truck id ${id} not found`,
@@ -65,14 +66,14 @@ app.get('/food-trucks/:id', async (req, res) => {
  */
 app.post('/food-trucks/search', async (req, res) => {
   const { searchValue } = req.body;
-  if (!searchFoodTruck) {
+  if (!searchValue) {
     res.status(400).send({
       message: 'Invalid Request',
     });
     return;
   }
   try {
-    const data = await searchFoodTruck(searchValue);
+    const data = await foodTruckService.searchFoodTruck(searchValue);
     res.status(200).send(data);
     return;
   } catch (e) {
